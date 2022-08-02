318a3280ee0037f7deff6572d8da151617c822d4
L-ING <hlf01@icloud.com>
2022-08-02 22:43:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 rc-service mariadb start
 mysqld_safe --datadir=/var/lib/mysql/ &
-sleep 5
+sleep 1
 mysql -e "use mysql;select User, host from mysql.user;grant all privileges on *.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWD' with grant option;flush privileges;"
 mysqladmin shutdown
 mysqld_safe --datadir=/var/lib/mysql
